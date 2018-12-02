2c177460ae5d202915ff6070758a086a601d8acb
Yoann Saunier <yoann.saunier@gmail.com>
2018-12-02 19:33:22 +0100

    Translated the original design

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Image,
   Platform,
@@ -7,59 +7,179 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Animated,
+  Dimensions,
+  Share,
 } from 'react-native';
-import { WebBrowser } from 'expo';
-
+import { WebBrowser, LinearGradient } from 'expo';
 import { MonoText } from '../components/StyledText';
+import { Avatar } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Ionicons';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import Autolink from 'react-native-autolink';
+
+const win = Dimensions.get('window');
+const HEADER_MAX_HEIGHT = win.width;
+const HEADER_MIN_HEIGHT = 60;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollY: new Animated.Value(0),
+      barTitleText: '1/1',
+      liked: false,
+      likes: 201,
+    };
+  }
+
+  static navigationOptions = { header: null };
 
   render() {
+    const headerHeight = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+      extrapolate: 'clamp',
+    });
+    const imageOpacity = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+      outputRange: [0.7, 0.5, 0],
+      extrapolate: 'clamp',
+    });
+    const imageTranslate = this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [0, -60],
+      extrapolate: 'clamp',
+    });
+
+    fade = (color1, color2) => this.state.scrollY.interpolate({
+      inputRange: [0, HEADER_SCROLL_DISTANCE],
+      outputRange: [color1, color2],
+      extrapolate: 'clamp',
+    });
+
+    changeBarTitleOnScroll = () => {
+      if (this.state.scrollY._value >= 250) {
+        this.setState({ barTitleText: 'What\'s the best mak...' });
+      } else {
+        this.setState({ barTitleText: '1/1' });
+      }
+    };
+
+    onShare = () => {
+      Share.share({
+        title: 'Male Up Tuition',
+        subject: 'What\'s the best makeup products for the tropical wheather like Thailand?',
+        url: 'https://www.ddlion.me',
+      }, {
+        dialogTitle: 'Share this article'
+      })
+    }
+
+    like = () => {
+      this.setState({ liked: !this.state.liked });
+      if (this.state.liked) {
+        this.setState({ likes: this.state.likes -= 1 });
+      } else {
+        this.setState({ likes: this.state.likes += 1 });
+      }
+    }
+
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+        <View>
+          <ScrollView
+             scrollEventThrottle={16}
+             onScroll={Animated.event(
+               [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}],
+               {listener: () => {changeBarTitleOnScroll()}}
+             )}
+           >
+            <View style={styles.scrollViewContent}>
+              <Text style={{fontSize: 15, color: '#fd5c63'}}>Make Up Tuition{"\n"}</Text>
+              <Text style={{fontSize: 15}}>
+                What's the best makeup products for the tropical wheather like Thailand?{"\n"}
+              </Text>
+              <Text style={{color: 'rgba(96,100,109, 1)'}}>
+                I wrote myself some 'guinea pig' notes on a recent 38°C scorcher
+                in Thailand (100.4°F) and had this to say about blush: "Hahahahaha.
+                My entire face is blush." Taking "less is more" as your perpetual
+                adage for the makup in tropical weather, here are my best tips,
+                one product at a time:
+                {"\n"}{"\n"}
+                Cruelty-free, totally natural & tested myself. As in the best
+                toiletries for Thailand, ecery product below 100% cruelty-free
+                (no animal testing). I've tried them nearly all of them personally,
+                bought myself as a normal customer - no freebies - during the
+                hottest wheather I've ever known on Koh Samui.
+                {"\n"}{"\n"}
+                1. <Autolink text="#Natural" hashtag="instagram" linkStyle={{color: '#fd5c63'}} /> Bamboo / Charcoal Blotting Sheet{"\n"}
+                2. BareMinerals Waterproof <Autolink text="#Mascara" hashtag="instagram" linkStyle={{color: '#fd5c63'}} />{"\n"}
+                3. <Autolink text="#SunBum" hashtag="instagram" linkStyle={{color: '#fd5c63'}} /> SPF Lip Balm{"\n"}
+                4. Coola Suncare Matte Tint Face Sunscreen{"\n"}
+                5. RMS Beauty Organis Makup Palette{"\n"}
+                6. EcoBrow defining wax{"\n"}
+                7. Drawstring Travel Makeup Organizer{"\n"}
+              </Text>
             </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
+          </ScrollView>
+          <Animated.View style={[styles.header, {height: headerHeight}]}>
+            <LinearGradient style={{height: '100%'}}
+              colors={[JSON.stringify(fade('black', 'transparent')), 'transparent']}
+              // colors={this.state.scrollY._value <= 250 ? ['black', 'transparent'] : ['transparent']}
+            >
+              <Animated.Image
+                source={require('../assets/images/photo-1512496015851-a90fb38ba796.jpg')}
+                style={[
+                  styles.backgroundImage,
+                  {opacity: imageOpacity, transform: [{translateY: imageTranslate}]},
+                ]}
+              />
+              <View style={styles.bar}>
+                <Animated.Text style={{color: fade('white', '#fd5c63')}}>
+                  <Icon size={24} name='ios-close-circle-outline' />
+                </Animated.Text>
+                <Animated.Text style={{color: fade('white', 'black'), fontWeight: 'bold', paddingTop: 4}}>
+                  {this.state.barTitleText}
+                </Animated.Text>
+                <Animated.Text style={{color: fade('white', '#fd5c63')}}>
+                  <FeatherIcon size={21} name='external-link' onPress={onShare} />
+                </Animated.Text>
+              </View>
+            </LinearGradient>
+          </Animated.View>
+        </View>
 
         <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
 
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+              <Avatar rounded source={require('../assets/images/128.jpg')}/>
+              <View style={{marginLeft: 6, height: 32}}>
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                  <Text style={{fontSize: 12}}>Lauren Levinson</Text>
+                  <Text style={styles.follow}>Follow</Text>
+                </View>
+                <Text style={styles.postDate}>5 Mins Ago</Text>
+              </View>
+            </View>
+
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center'}}>
+              <Icon name={this.state.liked ? 'ios-heart' : 'ios-heart-empty'}
+                    size={24}
+                    color='#fd5c63'
+                    onPress={like}
+                    />
+              <Text style={{color: '#fd5c63', marginLeft: 5}}>
+                {this.state.likes}
+              </Text>
+            </View>
+
           </View>
         </View>
+
       </View>
     );
   }
@@ -113,38 +233,12 @@ const styles = StyleSheet.create({
   contentContainer: {
     paddingTop: 30,
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
   welcomeImage: {
     width: 100,
     height: 80,
     resizeMode: 'contain',
     marginTop: 3,
     marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
   },
   tabBarInfoContainer: {
     position: 'absolute',
@@ -165,24 +259,64 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#fbfbfb',
     paddingVertical: 20,
+    paddingLeft: 14,
+    paddingRight: 16,
   },
-  tabBarInfoText: {
-    fontSize: 17,
+  follow: {
+    fontSize: 12,
+    color: '#fd5c63',
+    fontWeight: 'bold',
+    marginLeft: 5,
+  },
+  postDate: {
+    fontSize: 12,
     color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
   },
   helpLinkText: {
     fontSize: 14,
     color: '#2e78b7',
+  },
+  header: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    backgroundColor: '#fbfbfb',
+    overflow: 'hidden',
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+  bar: {
+    marginTop: 28,
+    marginLeft: 14,
+    marginRight: 14,
+    height: 32,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+  },
+  scrollViewContent: {
+    marginTop: HEADER_MAX_HEIGHT,
+    marginBottom: win.width / 4,
+    paddingTop: 14,
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  backgroundImage: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
   },
 });
